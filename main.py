e6ce93aa42f570c5df3b4884b4c7e8cb4c56ae28
Rajeev Warrier <warrier.rajeev.95@gmail.com>
2025-02-21 11:35:28 +0530

    add notebook

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import tiktoken
 from src.file_reader import FileReader, process_md, LOGGER
 from src.settings import MD_DIR_PATH, PROCESSED_DIR_PATH, CHUNK_SIZE
 from pathlib import Path
@@ -31,32 +30,3 @@ else:
     LOGGER.info("No MD files found")
 
 nodes = FileReader(input_dir=PROCESSED_DIR_PATH).load_data()
-
-tokenizer = tiktoken.encoding_for_model("gpt-3.5-turbo")
-prev_metadata = ""
-start = True
-for node in nodes:
-    inside_limit = False
-    metadata_str: str = "\n".join(
-        [f"{key}: {value}" for key, value in node.metadata.items()]
-    )
-    token_length = len(tokenizer.encode(node.text))
-    if token_length < ((CHUNK_SIZE - len(tokenizer.encode(metadata_str))) * 1.4):
-        inside_limit = True
-
-    if prev_metadata == node.metadata or start:
-        print(node.metadata)
-        print(f"Length of tokens: {token_length}")
-        print(f"Is chunk inside token limit?: {inside_limit}")
-        print(node.text)
-        print(100 * "-")
-    else:
-        print(100 * "=")
-        print(100 * "=")
-        print(node.metadata)
-        print(f"Length of tokens: {token_length}")
-        print(f"Is chunk inside token limit?: {inside_limit}")
-        print(node.text)
-        print(100 * "-")
-    start = False
-    prev_metadata = node.metadata
